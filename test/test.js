ff9d5fa521eca5b0a2fbaac1c87ecfb37ee6c173
Antonio <toony1908@gmail.com>
2017-01-26 23:39:11 -0600

    inicial

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var supertest = require("supertest");
+var should = require("should");
+
+var server = supertest.agent("http://localhost:3000");
+describe("SAMPLE unit test",function(){
+
+  it("should return home page",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(200) 
+    .end(function(err,res){
+      res.status.should.equal(200);
+      done();
+    });
+  });
+
+});
